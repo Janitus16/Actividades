b213a49dea0364a0bdab94554184b6cce136835b
Janitus16 <janitus16@gmail.com>
2026-08-17 02:33:48 +0200

    Primer ejercicio Realizado

--- C++/Punteros/001.cpp
+++ C++/Punteros/001.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+  int num;
+
+  std::cout << "Escriba un numero cualquiera: ";
+  std::cin >> num;
+  std::cout << "\n";
+  if (num != 0) {
+    if (num < 0) {
+      std::cout << "Su numero es negativo, ";
+    }
+    else {
+      std::cout << "Su numero es positivo, ";
+    }
+  }
+  if((num%2)==0) {
+    if (num == 0) {
+      std::cout << "Su numero es el cero, asi que no puede ser par/impar." << std::endl;
+    }
+
+    
+
+    if (num != 0) {
+      std::cout << "y es par.";
+    }
+  }
+
+  else {
+    std::cout << "y es impar.";
+  }
 }
